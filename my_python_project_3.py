c65d652434ed0dec1fd5a5a9f2c89761759cee1f
Barshon Saha Ayon <84730254+ayon121@users.noreply.github.com>
2021-11-16 16:58:56 +0600

    Create my_python_project_3.py

---
+++ my_python_project_3.py
@@ -0,0 +1,10 @@
+#It is my 7th python project 
+#In this project I will print even number from 0 to 50
+
+
+#i am using while loop for this 
+num = 0
+while num<=50:
+    print ( num)
+    num = num + 2
+
