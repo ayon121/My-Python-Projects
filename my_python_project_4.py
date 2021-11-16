0b3031f08d76649908588fc8e112e932dbc28323
Barshon Saha Ayon <84730254+ayon121@users.noreply.github.com>
2021-11-16 17:00:47 +0600

    Create my_python_project_4.py

---
+++ my_python_project_4.py
@@ -0,0 +1,8 @@
+#it is my 4th python project
+#this time i will print all odd number from 0 to 50
+
+#i am using while loop for this project
+num= 1
+while num <= 50:
+    print ( num )
+    num = num + 2
