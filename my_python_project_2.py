9969296ca19954b7f09ee1d185221c88ff4d3344
Barshon Saha Ayon <84730254+ayon121@users.noreply.github.com>
2021-11-16 16:56:27 +0600

    Create my_python_project_2.py

---
+++ my_python_project_2.py
@@ -0,0 +1,16 @@
+#it is my 2th python project
+#In this project I have 8 players
+#I have to fix match between them
+
+
+#first i made two team of player
+playerteam1= ["Jack" , "Rocky" , "Shuvo" , "Petter" ]
+playerteam2= ["Rafin", "Joy" , "Sohel" , "Mahi" ]
+
+
+#there i made a for loop 
+for i in playerteam1:
+    #there i mabe an another for loop in te for loop
+    
+    for e in playerteam2:
+        print (i, "will play with" ,e)
